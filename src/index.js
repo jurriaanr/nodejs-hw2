8e55abfbf8d8fad1cfa14259e7530fb2259a06a4
Jurriaan <jurriaanr@gmail.com>
2019-01-16 22:16:05 +0100

    console log colors

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import {httpServer, httpsServer} from './server'
 const httpPort = config.httpPort || 3000
 const httpsPort = config.httpsPort || 3001
 
-httpServer.listen(httpPort, () => console.log(`Listening on port ${httpPort}`))
-httpsServer.listen(httpsPort, () => console.log(`Listening on port ${httpsPort} for https`))
+httpServer.listen(httpPort, () => console.log('\x1b[33m%s\x1b[0m', `Listening on port ${httpPort}`))
+httpsServer.listen(httpsPort, () => console.log('\x1b[33m%s\x1b[0m', `Listening on port ${httpsPort} for https`))
